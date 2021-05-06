324e048682ddf13634e100d4bad9cca8b4d87e96
Andrea Bruno <andreabrunoxy@gmail.com>
2021-05-06 09:23:04 +0200

    form updated

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -10,7 +10,6 @@ export default function ContactForm() {
           name="contact"
           method="POST"
           action="/"
-          data-netlify-recaptcha="true"
           data-netlify="true"
         >
           <input type="hidden" name="form-name" value="contact" />
@@ -72,9 +71,6 @@ export default function ContactForm() {
               <label htmlFor="privacy"> Accetto la vostra Privacy Policy</label>
             </div>
             <br />
-          </p>
-          <div data-netlify-recaptcha="true"></div>
-          <p>
             <button className={styles.submitButton} type="submit">
               Invia
             </button>
