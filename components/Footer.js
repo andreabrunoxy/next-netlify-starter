f01b60e4ac2c90e1874228991768d34eb8378310
Andrea Bruno <andreabrunoxy@gmail.com>
2021-08-12 09:55:32 +0200

    add useOnScreen to Footer and subtitle

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,20 @@
-import styles from './Footer.module.css'
+import { useRef } from "react";
+import useOnScreen from "hooks/useOnScreen";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
+  /*useOnScreen*/
+  const ref = useRef(null);
+  // useEffect(() => {}, []);
+  const isVisible = useOnScreen(ref);
+  console.log("isVisible", isVisible);
+  /*End of useOnScreen*/
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+      <footer ref={ref} className={isVisible ? styles.footer : styles.prev}>
+        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />{" "}
+        for you
       </footer>
     </>
-  )
+  );
 }
